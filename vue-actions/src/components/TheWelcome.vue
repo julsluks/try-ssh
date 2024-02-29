3b4c62ad910ae0ec6e2706686cdd9d0bf974a951
julsluks <a22betvilver@inspedralbes.cat>
2024-02-29 10:37:58 +0100

    vue hecho

---
+++ vue-actions/src/components/TheWelcome.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Welcome to Vue Actions</h1>
+  <div class="button-container">
+    <button v-for="n in 9" :key="n" @click="showBtn(n)">{{ n }}</button>
+  </div>
+  <p>{{ btnNum }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      btnNum: ''
+    }
+  },
+  methods: {
+    showBtn(n) {
+      this.btnNum = n;
+      console.log('Button clicked:', n);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button-container {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+</style>
